221eb5c1f7efd5bafaa1aa14b45fb3d1d58924f1
xsagatof <Ultimate123443@gmail.com>
2024-11-01 23:59:28 +0500

    Updated Controllers

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebMVC.Data;
+using WebMVC.Models;
+
 namespace WebMVC
 {
 	public class Program
@@ -5,14 +10,23 @@ namespace WebMVC
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			builder.Services.AddDbContext<WebMVCContext>(options =>
+			    options.UseSqlServer(builder.Configuration.GetConnectionString("WebMVCContext") ?? throw new InvalidOperationException("Connection string 'WebMVCContext' not found.")));
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                SeedData.Initialize(services);
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
 			{
 				app.UseExceptionHandler("/Home/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
