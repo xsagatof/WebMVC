a0ff819611d3b2a75e8b0fa82319c341614ec18a
xsagatof <Ultimate123443@gmail.com>
2024-11-03 16:46:54 +0500

    Added Validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,7 +5,14 @@ namespace WebMVC.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 2)]
+    [Required]
     public string? Title { get; set; }
+
+    [RegularExpression(@"^[A-z]+[a-zA-z\s]*$")]
+    [Required]
+    [StringLength(30)]
     public string? Genre { get; set; }
     public string? Link { get; set; }
 }
