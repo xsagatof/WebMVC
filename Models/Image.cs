3294e6b060d197a99b55cd91ec6461a866b39bfa
xsagatof <Ultimate123443@gmail.com>
2024-11-10 22:51:44 +0500

    Added Image class

---
+++ Models/Image.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMVC.Models
+{
+	public class Image
+	{
+		public int Id { get; set; }
+		public string Url { get; set; }
+		public string Type { get; set; } //poster or fragments from movie
+		[ForeignKey("Movie")]
+		public int MovieId { get; set; }
+		public Movie Movie { get; set; }
+	}
+}
