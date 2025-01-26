05281abe8bfb75e716eba7de1ddc4d21e5d3e491
xsagatof <Ultimate123443@gmail.com>
2025-01-26 23:55:56 +0500

    Adding models for authenticaiton

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMVC.Dtos
+{
+	public class LoginDto
+	{
+		[Required(ErrorMessage = "Username is required")]
+		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Email is required")]
+		public string Password { get; set; }
+	}
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMVC.Models
+{
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
+		
+		[Required(ErrorMessage = "Username is required")]
+		[Column(TypeName = "nvarchar(100)")]
+		public string Username { get; set; }
+		
+		[Required(ErrorMessage = "Email is required")]
+		[Column(TypeName = "nvarchar(100)")]
+		public string Password { get; set; }
+		
+		[Required(ErrorMessage = "Email is required")]
+		[Column(TypeName = "nvarchar(100)")]
+		public string Email { get; set; }
+	}
+}
