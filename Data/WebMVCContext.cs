04e94760ed8f0e14376a3eb1807842178ec713ff
xsagatof <Ultimate123443@gmail.com>
2025-01-27 23:02:45 +0500

    Updated Data

--- Data/WebMVCContext.cs
+++ Data/WebMVCContext.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebMVC.Models;
 
 namespace WebMVC.Data
 {
-    public class WebMVCContext : DbContext
+    public class WebMVCContext : IdentityDbContext<User>
     {
         public WebMVCContext (DbContextOptions<WebMVCContext> options)
             : base(options)
@@ -15,7 +16,6 @@ namespace WebMVC.Data
         }
 
         public DbSet<Movie> Movie { get; set; } = default!;
-        public DbSet<User> Users { get; set; }
 
     }
 }
