1a2d4f44495448008aac8d49347395e5586f0e80
xsagatof <Ultimate123443@gmail.com>
2026-02-12 23:56:53 +0500

    User Profile feature starting point

---
+++ Models/UserProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebMVC.Models
+{
+	public class UserProfileViewModel
+	{
+		public string UserId { get; set; }
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public string FullName { get; set; }
+		public string PhoneNumber { get; set; }
+		public string ProfileImageUrl { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public string Address { get; set; }
+	}
+}
